6199c9f9b5ffc2dbb6d48eef7e04eb24d5b534c7
JoungSik <tjstlr2010@gmail.com>
2018-09-25 12:57:19 +0900

    Bug Fixed. Closed devise password_controller

--- app/controllers/v1/passwords_controller.rb
+++ app/controllers/v1/passwords_controller.rb
@@ -1,2 +1,9 @@
 class V1::PasswordsController < Devise::PasswordsController
+    def respond_with(resource, opts = {})
+        puts request.headers['Accept'] != 'application/json' 
+        if request.headers['Accept'] != 'application/json'
+        # if request.method != 'POST'
+            render json: { errors: 'Not supports.' }
+        end
+    end
 end
