d6cd8d02e9de811f82ef75bd834af3dcbc2843a6
JoungSik <tjstlr2010@gmail.com>
2018-09-27 23:14:46 +0900

    [#160705263 Finishes] Add new friend API

--- app/controllers/v1/friend_controller.rb
+++ app/controllers/v1/friend_controller.rb
@@ -17,5 +17,32 @@ class V1::FriendController < ApplicationController
   end
 
   def new
+    current_user = checkUser(request)
+    unless current_user.nil?
+      user_ids = params['user_ids']
+      if !user_ids.nil?
+        @friends = Array.new
+        user_ids.each do |user_id|
+          check = Friend.check_friend(current_user.id, user_id).first
+          if check.nil?
+            friend = Friend.new
+            friend.request_user_id = current_user.id
+            friend.response_user_id = user_id
+            friend.assent = false
+            friend.save
+
+            @friends.push(User.find(user_id))
+          end
+        end
+      else
+        render json: {
+          message: { code: 400, message: "No have users id" }
+        }, status: 400
+      end
+    else
+      render json: {
+        message: { code: 401, message: "Unauthorized auth_token." }
+      }, status: 401
+    end
   end
 end
--- app/models/friend.rb
+++ app/models/friend.rb
@@ -4,4 +4,8 @@ class Friend < ActiveRecord::Base
     def self.get_friends(user_id)
         where('request_user_id = ? or response_user_id = ?', user_id, user_id).where(assent: true)
     end
+
+    def self.check_friend(my_id, user_id)
+        where('(request_user_id = ? and response_user_id = ?) or (request_user_id = ? and response_user_id = ?)', my_id, user_id, user_id, my_id)
+    end
 end
