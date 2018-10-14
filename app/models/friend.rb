e1c1167b4fa5bf7594fbdee3aaf80664ff36509b
JoungSik <tjstlr2010@gmail.com>
2018-10-14 22:45:36 +0900

    Bug Fixed. Friends Search API bug fixed.

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -2,12 +2,13 @@ class Friend < ActiveRecord::Base
 
   def self.get_friends(user_id)
     select("users.*, friends.assent, (friends.request_user_id = #{user_id}) as request")
-        .joins("join users on users.id = #{user_id}")
+        .joins("join users on users.id = IF (friends.request_user_id = #{user_id}, friends.response_user_id, friends.request_user_id)")
         .where('friends.request_user_id = ? or friends.response_user_id = ?', user_id, user_id)
 
-    # SELECT friends.assent, (friends.request_user_id = 1) as request, users.*
-    # FROM friends
-    # JOIN users ON users.id = 1;
+    # select users.*, friends.assent, (friends.request_user_id = 1) as request
+    # from friends
+    # join users on users.id = if (friends.request_user_id = 1, friends.response_user_id, friends.request_user_id)
+    # where friends.request_user_id = 1 or friends.response_user_id = 1;
   end
 
   def self.check_friend(my_id, user_id)
